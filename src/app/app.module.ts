f1caf51d45aa287e95bd378d958aaff4e3d2ac4b
stouf <ouadad.mustapha01@gmail.com>
2025-04-29 10:54:17 +0100

    install  bootstrap/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,11 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp({"projectId":"flavorshare-222f4","appId":"1:76917660470:web:4439321a8d35587a973197","storageBucket":"flavorshare-222f4.firebasestorage.app","apiKey":"AIzaSyBgmfXcOa5T6kYoI6bL0uNXvA1hTiHZBLk","authDomain":"flavorshare-222f4.firebaseapp.com","messagingSenderId":"76917660470"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
